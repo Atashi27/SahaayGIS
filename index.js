8439bbc74f49e8799e160b7fcd41d82b4c589618
Atashi <atashi.khatua@speedboxapp.com>
2019-03-13 21:29:53 +0530

    Heroku changes

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ var express = require("express");
 var pg = require("pg");
 var app = express();
 var path = require("path");
- var bodyParser = require('body-parser');
- var session=require('express-session');
- var config = {
-   user: 'postgres',
-   database: 'postgres',
-   password: 'star',
-   port: 5432,
-   max: 10, // max number of connection can be open to database
-   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
- };
- app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
- var pool = new pg.Pool(config);
- app.use(bodyParser.json());
- app.use(bodyParser.urlencoded({ extended: false }));
+var bodyParser = require('body-parser');
+var session = require('express-session');
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.postgres://kdtyywrerrwvie:80ce2ac1ab1f8f9c7a63b0301d9514adfe9f8d6187b59913a6dcb63d0e3e8537@ec2-23-23-241-119.compute-1.amazonaws.com:5432/d61fs4ghbsj3g5,
+  ssl: true
+});
+var config = {
+  user: 'postgres',
+  database: 'postgres',
+  password: 'star',
+  port: 5432,
+  max: 10, // max number of connection can be open to database
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
+app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: true }));
+// var pool = new pg.Pool(config);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 var sess;
 app.use("/images", express.static(__dirname + "/public/images"));
 app.use("/data", express.static(__dirname + "/public/data"));
@@ -23,31 +28,44 @@ app.use("/header", express.static(__dirname + "/public/pages/header.html"));
 app.use("/header1", express.static(__dirname + "/public/pages/header1.html"));
 
 app.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
 app.get("/nearesthospital", function(req, res) {
-	res.sendFile(path.join(__dirname + "/public/pages/nearesthospital.html"));
+  res.sendFile(path.join(__dirname + "/public/pages/nearesthospital.html"));
 });
 
 app.get("/nearestpharmacy", function(req, res) {
-	res.sendFile(path.join(__dirname + "/public/pages/nearestpharmacy.html"));
+  res.sendFile(path.join(__dirname + "/public/pages/nearestpharmacy.html"));
 });
 
 app.get("/tips", function(req, res) {
-	res.sendFile(path.join(__dirname + "/public/pages/tips.html"));
+  res.sendFile(path.join(__dirname + "/public/pages/tips.html"));
 });
 
 app.get("/feedback", function(req, res) {
-	res.sendFile(path.join(__dirname + "/public/pages/feedback.html"));
+  res.sendFile(path.join(__dirname + "/public/pages/feedback.html"));
 });
 
 app.get("/signup", function(req, res) {
-	res.sendFile(path.join(__dirname + "/public/pages/signup.html"));
+  res.sendFile(path.join(__dirname + "/public/pages/signup.html"));
 });
 
 app.get("/login", function(req, res) {
-	res.sendFile(path.join(__dirname + "/public/pages/login.html"));
+  res.sendFile(path.join(__dirname + "/public/pages/login.html"));
 });
 
 app.get("/dashboard", function(req, res) {
@@ -84,95 +102,94 @@ app.get("/nearestpharmacy1", function(req, res) {
 
 
 
- app.post('/users', function(req, res) {
-   pool.connect(function(err, client, done) {
-     if (err) {
-       console.log("not able to get connection " + err);
-       res.status(400).send(err);
-     }
-     client.query('INSERT INTO users(name, email, password, bloodgrp, age, gender, allergies, address, contact) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)', [req.body.name, req.body.email, req.body.password, req.body.bloodgrp, req.body.age, req.body.gender, req.body.allergies, req.body.address, req.body.contact]);
-     //call `done()` to release the client back to the pool
-     done();
-     res.redirect('/login');
-   });
- });
+app.post('/users', function(req, res) {
+  pool.connect(function(err, client, done) {
+    if (err) {
+      console.log("not able to get connection " + err);
+      res.status(400).send(err);
+    }
+    client.query('INSERT INTO users(name, email, password, bloodgrp, age, gender, allergies, address, contact) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)', [req.body.name, req.body.email, req.body.password, req.body.bloodgrp, req.body.age, req.body.gender, req.body.allergies, req.body.address, req.body.contact]);
+    //call `done()` to release the client back to the pool
+    done();
+    res.redirect('/login');
+  });
+});
 
-app.post('/feedback',function(req, res){
-	
-	pool.connect(function(err,client,done) {
-       if(err){
-           console.log("not able to get connection "+ err);
-           res.status(400).send(err);
-       } 
-       client.query('INSERT INTO feedback(name, email, comment) VALUES($1, $2, $3)',[req.body.name, req.body.email, req.body.comment]);
-          //call `done()` to release the client back to the pool
-           done(); 
-           res.redirect('/');
-    
-    });
-    });
-	app.post('/add',function(req, res){
-	 pool.connect(function(err,client,done) {
-	 var email= req.body.email;
-	 var password = req.body.password;
-	 client.query('SELECT email,password FROM users WHERE email = $1 AND password =$2',[email,password],function(error,results){
-		// console.log(email);
-	//console.log(password);
-	
-    		 
-		 if (error) {
-    res.send({
-      "code":400,
-      "failed":"error ocurred"
-    })
-  }else{
-         if(results.rowCount==1){
-				sess=req.session;	
-				sess.email=req.body.email;
-	
-				done();
-				res.redirect('/dashboard');
-				
-				app.post('/feedback1',function(req, res){
-					if(err){
-						console.log("not able to get connection "+ err);
-						res.status(400).send(err);
-					} 
-					client.query('INSERT INTO service_feedback(name, email, rating, suggestions) VALUES($1, $2, $3, $4)',[req.body.name, email, req.body.rating,req.body.suggestions]);
-					//call `done()` to release the client back to the pool
-					
-						res.redirect('/dashboard');
-				});
-				app.post('/sub',function(req, res){
-                const accountSid = 'AC8f2597d0199bedbdbb4f98203d77e21e';
-                const authToken = 'd662dfb87866e64826ac15babb662db3';
-                client = require('twilio')(accountSid, authToken);
-			
-                client.messages
-                .create({
-                body: 'Emergency!!'+email,
+app.post('/feedback', function(req, res) {
+
+  pool.connect(function(err, client, done) {
+    if (err) {
+      console.log("not able to get connection " + err);
+      res.status(400).send(err);
+    }
+    client.query('INSERT INTO feedback(name, email, comment) VALUES($1, $2, $3)', [req.body.name, req.body.email, req.body.comment]);
+    //call `done()` to release the client back to the pool
+    done();
+    res.redirect('/');
+
+  });
+});
+app.post('/add', function(req, res) {
+  pool.connect(function(err, client, done) {
+    var email = req.body.email;
+    var password = req.body.password;
+    client.query('SELECT email,password FROM users WHERE email = $1 AND password =$2', [email, password], function(error, results) {
+      // console.log(email);
+      //console.log(password);
+
+
+      if (error) {
+        res.send({
+          "code": 400,
+          "failed": "error ocurred"
+        })
+      } else {
+        if (results.rowCount == 1) {
+          sess = req.session;
+          sess.email = req.body.email;
+
+          done();
+          res.redirect('/dashboard');
+
+          app.post('/feedback1', function(req, res) {
+            if (err) {
+              console.log("not able to get connection " + err);
+              res.status(400).send(err);
+            }
+            client.query('INSERT INTO service_feedback(name, email, rating, suggestions) VALUES($1, $2, $3, $4)', [req.body.name, email, req.body.rating, req.body.suggestions]);
+            //call `done()` to release the client back to the pool
+
+            res.redirect('/dashboard');
+          });
+          app.post('/sub', function(req, res) {
+            const accountSid = 'AC8f2597d0199bedbdbb4f98203d77e21e';
+            const authToken = 'd662dfb87866e64826ac15babb662db3';
+            client = require('twilio')(accountSid, authToken);
+
+            client.messages
+              .create({
+                body: 'Emergency!!' + email,
                 from: '+18647131453',
                 to: '+918668626097'
-                })
-                .then(message => console.log(message.sid));
- 
-});   
+              })
+              .then(message => console.log(message.sid));
+
+          });
+        } else {
+          res.send({
+            "code": 204,
+            "success": "Email and password does not match"
+          });
+        }
       }
-      else{
-        res.send({
-          "code":204,
-          "success":"Email and password does not match"
-            });
-      }
-    }
-     
+
+    });
   });
-});   
 });
-	
+
 let port = process.env.PORT;
 if (port == null || port == "") {
-	port = 8000;
+  port = 8000;
 }
 app.listen(port);
 
