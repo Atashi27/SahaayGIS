7c98e966cb47a02fe67cc2670b373673137d0968
Atashi <atashi.khatua@speedboxapp.com>
2019-03-13 22:21:48 +0530

    Heroku changes(3)

--- index.js
+++ index.js
@@ -31,6 +31,20 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null };
+    res.render('pages/db', results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
+
 app.get("/nearesthospital", function(req, res) {
   res.sendFile(path.join(__dirname + "/public/pages/nearesthospital.html"));
 });
